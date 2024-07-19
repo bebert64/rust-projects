26d062003585472200a008eaa4dcb77e8db3ddfb
RomainC <bebert64@gmail.com>
2024-07-19 16:20:21 +0200

    one less alloc

--- libs/ConfigHelpers/src/lib.rs
+++ libs/ConfigHelpers/src/lib.rs
@@ -25,28 +25,38 @@ pub fn build_config<'de, CONFIG: serde::Deserialize<'de>>(
         .ok_or_don_err("Can't get base dirs")?
         .config_dir()
         .join(&format!("by_db/{}.toml", app_name));
+    let config_in_working_dir_path = format!("{}.toml", app_name);
     let builder = Config::builder()
         .add_source(File::with_name(&config_in_home_path.to_string_lossy()).required(false))
-        .add_source(File::with_name(&format!("{}.toml", app_name)).required(false))
+        .add_source(File::with_name(&config_in_working_dir_path).required(false))
         .add_source(Environment::with_prefix(&app_name.to_uppercase()));
     let config_incomplete = builder.build_cloned().expect("Should be a valid config");
     Ok(builder
-    .set_override("postgres.name", {
-        if config_incomplete.get_string("mode").unwrap_or_default().to_lowercase() == *"prod" {
-            config_incomplete.get_string("postgres.name_prod").unwrap_or_default()
-        } else {
-            config_incomplete.get_string("postgres.name_dev").unwrap_or_default()
-        }
-    })
-    .expect("Key postgres.name should exist")
-    .build()
-    .expect("Should be a valid config")
-    .try_deserialize()
-    .unwrap_or_else(|err|
-        panic!(
-            "Something went wrong while deserializing config. Trying to read from {:?} and {:?}. Err: {err:?}",
-            config_in_home_path,
-            &format!("{}.toml", app_name)
-        )
-    ))
+        .set_override("postgres.name", {
+            if config_incomplete
+                .get_string("mode")
+                .unwrap_or_default()
+                .to_lowercase()
+                == *"prod"
+            {
+                config_incomplete
+                    .get_string("postgres.name_prod")
+                    .unwrap_or_default()
+            } else {
+                config_incomplete
+                    .get_string("postgres.name_dev")
+                    .unwrap_or_default()
+            }
+        })
+        .expect("Key postgres.name should exist")
+        .build()
+        .expect("Should be a valid config")
+        .try_deserialize()
+        .unwrap_or_else(|err| {
+            panic!(
+                "Something went wrong while deserializing config. 
+                Trying to read from {config_in_home_path:?} and {config_in_working_dir_path:?}.
+                Err: {err:?}",
+            )
+        }))
 }
